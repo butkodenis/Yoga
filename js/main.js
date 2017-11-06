504d789905058466c1fc977d1f76008a6cead374
butkodenis <Butko.dv@gmail.com>
2017-11-06 09:46:11 +0200

    start html/css/scss

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(window).load(function() {
+    $('.flexslider').flexslider({
+
+        animation: "slide"              //String: Select your animation type, "fade" or "slide"
+        // smoothHeight: false,            //{NEW} Boolean: Allow height of the slider to animate smoothly in horizontal mode
+        // slideshow: false,                //Boolean: Animate slider automatically
+        // controlNav: false,
+        // directionNav: true,             //Boolean: Create navigation for previous/next navigation? (true/false)
+        // prevText: "%",           //String: Set the text for the "previous" directionNav item
+        // nextText: "&",
+        // Carousel Options
+
+    });
+});
