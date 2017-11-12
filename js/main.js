fd404c17d15b7ef63f243bf29afa7bb9f74f4858
butkodenis <Butko.dv@gmail.com>
2017-11-12 13:47:53 +0200

    up

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 $.noConflict();
 jQuery(document).ready(function ($) {
-
+    // валидация
     $("#form").validetta({
 
 
     });
+    // $( "#link" ).hover(
+    //     function() {
+    //         $(".defis" ).toggleClass( ".defis_active" ) ;
+    //     }
+    // );
+
+    // $("#link").mouseout(function(e)
+    // {
+    //     $(".defis"). css("color", "red");
+    // });
+    $("#link").hover(function () {
+        $(this).next(".defis"). toggleClass("defis_active");
+    });
+
+    // $("#link").mousemove(function(e)
+    // {
+    //     $(".defis").css("color", "#fff");
+    // });
+
     // плавная прокрутка
     $('a[href^="#"]').click(function(){
 //Сохраняем значение атрибута href в переменной:
@@ -12,6 +31,8 @@ jQuery(document).ready(function ($) {
         $('html, body').animate({scrollTop: $(target).offset().top}, 1500);
         return false;
     });
+
+
     // слайдер slick
     $('.slider-for').slick({
         slidesToShow: 1,
@@ -109,71 +130,71 @@ jQuery(document).ready(function ($) {
 
 
    //slider
-    if ($('.owl-carousel').length > 0) {
-        (function () {
-            //слайдер с коментариями
-            $('.carusel_1').owlCarousel({
-                loop:true,
-                // margin:10,
-                navText : ["",""],
-                autoplay: true,
-                nav:true,
-                center: true,
-                smartSpeed:1000, //Время движения слайда
-                autoplayTimeout:6000, //Время смены слайда
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    600:{
-                        items:1
-                    },
-                    1000:{
-                        items:1
-                    }
-                }
-            });
-            // слайдер с логотипами
-            $('.slide-partners').owlCarousel({
-                loop:true, //Зацикливаем слайдер
-                // margin:10, //Отступ от картино если выводите больше 1
-                nav:false, //Отключил навигацию
-                autoplay:true, //Автозапуск слайдера
-                smartSpeed:2000, //Время движения слайда
-                autoplayTimeout:4000, //Время смены слайда
-                responsive:{ //Адаптация в зависимости от разрешения экрана
-                    0:{
-                        items:5
-                    },
-                    600:{
-                        items:5
-                    },
-                    1000:{
-                        items:5
-                    }
-                }
-            });
-
-            // слайдер с блогами
-            $('.slide-blog').owlCarousel({
-                loop:true,
-                margin:10,
-                nav:true,
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    600:{
-                        items:3
-                    },
-                    1000:{
-                        items:3
-                    }
-                }
-            });
-
-        })();
-    }
+   //  if ($('.owl-carousel').length > 0) {
+   //      (function () {
+   //          //слайдер с коментариями
+   //          $('.carusel_1').owlCarousel({
+   //              loop:true,
+   //              // margin:10,
+   //              navText : ["",""],
+   //              autoplay: true,
+   //              nav:true,
+   //              center: true,
+   //              smartSpeed:1000, //Время движения слайда
+   //              autoplayTimeout:6000, //Время смены слайда
+   //              responsive:{
+   //                  0:{
+   //                      items:1
+   //                  },
+   //                  600:{
+   //                      items:1
+   //                  },
+   //                  1000:{
+   //                      items:1
+   //                  }
+   //              }
+   //          });
+   //          // слайдер с логотипами
+   //          $('.slide-partners').owlCarousel({
+   //              loop:true, //Зацикливаем слайдер
+   //              // margin:10, //Отступ от картино если выводите больше 1
+   //              nav:false, //Отключил навигацию
+   //              autoplay:true, //Автозапуск слайдера
+   //              smartSpeed:2000, //Время движения слайда
+   //              autoplayTimeout:4000, //Время смены слайда
+   //              responsive:{ //Адаптация в зависимости от разрешения экрана
+   //                  0:{
+   //                      items:5
+   //                  },
+   //                  600:{
+   //                      items:5
+   //                  },
+   //                  1000:{
+   //                      items:5
+   //                  }
+   //              }
+   //          });
+   //
+   //          // слайдер с блогами
+   //          $('.slide-blog').owlCarousel({
+   //              loop:true,
+   //              margin:10,
+   //              nav:true,
+   //              responsive:{
+   //                  0:{
+   //                      items:1
+   //                  },
+   //                  600:{
+   //                      items:3
+   //                  },
+   //                  1000:{
+   //                      items:3
+   //                  }
+   //              }
+   //          });
+   //
+   //      })();
+   //  }
 
 
 
